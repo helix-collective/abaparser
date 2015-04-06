f129004e3ddec27ceff8fcb1d8fccb6abccaca45
Jeeva Suresh <jeeva@helixta.com.au>
2015-04-06 15:11:33 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='abaparser',
+    version='0.1',
+    py_modules=['abaparser'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
